658b6d52dc21543395756967beb219620790ea1a
AldoJDev <aldojunio.dev@gmail.com>
2025-10-16 19:41:52 -0300

    feat(api): Swagger and delay between requests

--- api/src/main/java/com/tagview/challenge_api/controller/ProdutoController.java
+++ api/src/main/java/com/tagview/challenge_api/controller/ProdutoController.java
@@ -6,6 +6,7 @@ import com.tagview.challenge_api.repository.ProdutoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import java.util.concurrent.TimeUnit;
 
 import java.util.List;
 
@@ -13,22 +14,37 @@ import java.util.List;
 @RequestMapping("/api/v1/produtos")
 public class ProdutoController {
 
+    private void simularDelay() {
+        try {
+            //3 segundos
+            TimeUnit.SECONDS.sleep(3);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            //lidar com a exceção se a thread for interrompida.
+        }
+    }
+
     @Autowired
     private ProdutoRepository produtoRepository;
 
     @GetMapping("/test")
     public ResponseEntity<String> test() {
+        simularDelay();
         return ResponseEntity.ok("okok");
     }
 
     @GetMapping
     public ResponseEntity<List<Produto>> getAllProducts() {
+        simularDelay();
         return ResponseEntity.ok(produtoRepository.findAll());
     }
 
     @PostMapping
     public ResponseEntity<Produto> createProduct(@RequestBody Produto produto) {
+        simularDelay();
         Produto savedProduct = produtoRepository.save(produto);
         return ResponseEntity.ok(savedProduct);
     }
+
+
 }
